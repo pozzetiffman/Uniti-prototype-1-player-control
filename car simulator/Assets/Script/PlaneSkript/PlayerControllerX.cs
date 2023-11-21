9e0c39f3b5f73ac5f45a8ce57e0b09cec4b415a2
Mykyta Lvov <mykytalvov@gmail.com>
2023-11-21 13:14:31 +0300

    changeable variable

--- car simulator/Assets/Script/PlaneSkript/PlayerControllerX.cs
+++ car simulator/Assets/Script/PlaneSkript/PlayerControllerX.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed = 20f;
-    public float rotationSpeed = 20f;
+    public float Speed = 20f;
+    public float RotationSpeed = 20f;
 
     //Update вызывается каждый кадр, а FixedUpdate с фиксированной частотой
     void Update()
@@ -17,10 +17,10 @@ public class PlayerControllerX : MonoBehaviour
 
 
         // наклонить самолет вверх/вниз с помощью клавиш со стрелками вверх/вниз
-        transform.Rotate(Vector3.right * rotationSpeed * h * Time.deltaTime);
+        transform.Rotate(Vector3.right * RotationSpeed * h * Time.deltaTime);
 
         // перемещаем плоскость вперед с постоянной скоростью
-        transform.Translate(Vector3.forward * speed * v * Time.deltaTime);
+        transform.Translate(Vector3.forward * Speed * v * Time.deltaTime);
 
        
 
